c14d212411ed7bfa7c272233f79b10ca3bd3fa4c
Goodnessukaigwe <goodnessukaigwe2020@gmail.com>
2026-03-27 02:12:27 +0100

    fix(migrations): add ingest merge migration to unblock CI on branch 171

---
+++ django-backend/soroscan/ingest/migrations/0018_merge_webhook_timeout_and_eventdedup.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("ingest", "0016_webhooksubscription_timeout_seconds"),
+    ]
+
+    operations = []
