a6d2bed874181c6c156e6be3f8bc14d4af504231
Goodnessukaigwe <goodnessukaigwe2020@gmail.com>
2026-03-27 02:37:55 +0100

    Fix ingest migration conflict on deprecation branch

---
+++ django-backend/soroscan/ingest/migrations/0017_eventdeduplicationlog.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("ingest", "0015_merge_notification_and_teams"),
+    ]
+
+    operations = []
